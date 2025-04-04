6243966f555c6a3abaf70adc9dac08b2a97c2236
Lalitha-Kalle <kallelalitha04@gmail.com>
2025-04-04 10:35:18 +0530

    city-service completed

---
+++ src/services/city-service.js
@@ -0,0 +1,47 @@
+const CityRepository = require("../repository/city-repository")
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data);
+      return city;
+    } catch(error) {
+      console.log("something went wrong in service layer");
+      throw(error);
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCityCity(cityId);
+      return response;
+    } catch(error) {
+      console.log("something went wrong in service layer");
+      throw(error);
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await this.cityRepository.updateCity(cityId, data);
+      return city;
+    } catch(error) {
+      console.log("something went wrong in service layer");
+      throw(error);
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(data);
+      return city;
+    } catch(error) {
+      console.log("something went wrong in service layer");
+      throw(error);
+    }
+  }
+}
