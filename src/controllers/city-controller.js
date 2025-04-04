9da0c5941a10ff0addbe1fecdd752bf1d0438fb1
Lalitha-Kalle <kallelalitha04@gmail.com>
2025-04-04 11:14:37 +0530

    completed city-controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,90 @@
+const CityService = require("../services/city-service")
+const cityService = new CityService()
+
+
+/**
+ * post -> req.body
+ */
+const create = async (req, res) => {
+  try {
+    const city = await cityService.createCity(req.body);
+    return res.status(201).json({
+      data: city,
+      sucess: true,
+      message: "Successfully created a city",
+      err: {}
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to create a city",
+      err: error
+    })
+  }
+}
+
+//delete -> /city/:id
+
+const destroy = async (req, res) => {
+  try {
+    const response = await cityService.deleteCity(req.params.id);
+    return res.status(200).json({
+      data: response,
+      succes: true,
+      message: "Successfully deleted a city",
+      err: {}
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to delete the city",
+      err: error
+    })
+  }
+}
+
+//patch /city/:id , req.body - data
+const update = async (req, res) =>{
+  try {
+    const city = await cityService.updateCity(req.params.id, req.body);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: "Successfully updated city",
+      err: {}
+    })
+  } catch(error) {
+    console.log(error)
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "not able to update the city",
+      err: error
+    })
+  }
+}
+
+//get /city/:id
+const get = async (req, res) => {
+  try{
+    const city = await cityService.getCity(req.params.id);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: "Successfully fetched city",
+      err: {}
+    })
+  } catch(error) {
+    console.log(error)
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "not able to update the city",
+      err: error
+    })
+  }
+}
