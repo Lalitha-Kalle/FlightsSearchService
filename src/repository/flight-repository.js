514bb508518f7965891d4e408143b0038d6a80da
Lalitha-Kalle <kallelalitha04@gmail.com>
2025-04-05 10:58:34 +0530

    added get flights with new filters

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,6 +1,42 @@
 const { Flight } = require('../models/index')
 
+const { Op, where } = require("sequelize")
+
 class FlightRepository {
+
+  #createFilter(data) {
+    let filter = {};
+    if(data.arrivalAirportId) {
+      filter.arrivalAirportId = data.arrivalAirportId;
+    }
+    if(data.departureAirportId) {
+      filter.departureAirportId = data.departureAirportId;
+    }
+
+    let priceFilter = [];
+
+    if(data.minPrice) {
+      priceFilter.push({
+        price:{
+          [Op.gte] : data.minPrice
+        }
+      })
+    }
+
+    if(data.maxPrice) {
+      priceFilter.push({
+        price: {
+          [Op.lte]: data.maxPrice
+        }
+      })
+    }
+
+    Object.assign(filter, {[Op.and]: priceFilter})
+    console.log(filter)
+    return filter;
+  }
+
+
   async createFlight(data) {
     try {
       const flight = await Flight.create(data);
@@ -10,6 +46,19 @@ class FlightRepository {
       throw {error};
     }
   }
+
+  async getAllFlights(filter) {
+    try {
+      const filterObject = this.#createFilter(filter);
+      const flight = await Flight.findAll({
+        where: filterObject
+      })
+    } catch (error) {
+      console.log("Something went wrong in flightrepository");
+      throw {error};
+    }
+  }
+
 }
 
 module.exports = FlightRepository
